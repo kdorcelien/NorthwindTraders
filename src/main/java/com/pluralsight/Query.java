1f886cc19e6831daff181226b74285186e40b885
kdorcelien <kdorcelien.kd@gmail.com>
2025-12-01 14:53:26 -0500

    commit 3

--- src/main/java/com/pluralsight/Query.java
+++ src/main/java/com/pluralsight/Query.java
@@ -3,22 +3,43 @@ package com.pluralsight;
 import java.sql.*;
 
 public class Query {
-    public static void main(String[] args) throws SQLException,ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind","root",
-        "Olie_Corsaire001!");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root",
+                "Olie_Corsaire001!");
 
-Statement statement = connection.createStatement();
+        String query = "SELECT ProductId, ProductName, UnitPrice, UnitsInStock FROM Products WHERE ProductId = ? OR ProductName LIKE ?";
 
-String query = "SELECT ProductName FROM Products";
 
-ResultSet results = statement.executeQuery(query);
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
 
-while (results.next()){
-    String name = results.getString(1);
-    System.out.println(name);
-}
-connection.close();
+     statement.setInt(1,2);
+     statement.setString(2, "%hot%");
+
+
+
+            ResultSet results = statement.executeQuery();
+
+
+
+            while (results.next()) {
+                int id = results.getInt("ProductId");
+                String name = results.getString("ProductName");
+                int uPrice = results.getInt("UnitPrice");
+                int uInstock = results.getInt("UnitsInStock");
+                System.out.printf("Id:   %d %n Name:   %s %n Price:   %d %n Stock:   %d %n",id ,name, uPrice, uInstock);
+                System.out.println("--------------------------------------------------------");
+            }
+
+            results.close();
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
