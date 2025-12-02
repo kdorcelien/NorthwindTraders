23bb527a44b6a2fc94b7b0efbdb9cf61b96355cb
kdorcelien <kdorcelien.kd@gmail.com>
2025-12-02 11:38:42 -0500

    commit 4

--- src/main/java/com/pluralsight/Query.java
+++ src/main/java/com/pluralsight/Query.java
@@ -1,45 +1,131 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Query {
+    final static Scanner scan = new Scanner(System.in);
+    static Connection connection;
+    static Statement statement;
+    static ResultSet results;
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+try {
+    openConnection(args);
+    menuDisplay();
+} catch (SQLException e) {
+    throw new RuntimeException(e);
+} catch (ClassNotFoundException e) {
+    throw new RuntimeException(e);
+}finally {
+    closeConnection();
+}
+
+
+    }
+
+    public static void openConnection(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/northwind",
+                args[0],
+                args[1]
+        );
+    }
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", args[0],
-                args[1]);
+    public static void menuDisplay() throws SQLException, ClassNotFoundException {
+        System.out.println("What do you want to do?\n" +
+                "1) Display all products\n" +
+                "2) Display all customers\n" +
+                "0) Exit\n" +
+                "Select an option: ");
 
-        String query = "SELECT ProductId, ProductName, UnitPrice, UnitsInStock FROM Products WHERE ProductId = ? OR ProductName LIKE ?";
+        int option = scan.nextInt();
+        scan.nextLine();
 
 
+        switch (option) {
+            case 1 -> {
+                displayProducts();
+            }
+            case 2 -> displayCustomers();
+            case 0 -> {
+                System.out.println("Au-Revoir");
+                scan.close();
+                closeConnection();
+            }
+        }
+    }
+
+    public static void displayProducts() throws SQLException, ClassNotFoundException {
+        String query = "SELECT * FROM Products";
         try {
-            PreparedStatement statement = connection.prepareStatement(query);
 
-     statement.setInt(1,2);
-     statement.setString(2, "%hot%");
-
-
-
-            ResultSet results = statement.executeQuery();
-
-
+            statement = connection.createStatement();
+            results = statement.executeQuery(query);
 
             while (results.next()) {
                 int id = results.getInt("ProductId");
                 String name = results.getString("ProductName");
                 int uPrice = results.getInt("UnitPrice");
                 int uInstock = results.getInt("UnitsInStock");
-                System.out.printf("Id:   %d %n Name:   %s %n Price:   %d %n Stock:   %d %n",id ,name, uPrice, uInstock);
+                System.out.printf("Id:   %d %n Name:   %s %n Price:   %d %n Stock:   %d %n", id, name, uPrice, uInstock);
                 System.out.println("--------------------------------------------------------");
             }
-
-            results.close();
-            statement.close();
-            connection.close();
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
+
+
+    public static void displayCustomers() throws SQLException, ClassNotFoundException {
+        String query = "SELECT * FROM Customers ORDER BY Country";
+        try {
+
+            statement = connection.createStatement();
+
+
+            results = statement.executeQuery(query);
+
+            while (results.next()) {
+                String contactName = results.getString("ContactName");
+                String companyName = results.getString("CompanyName");
+                String city = results.getString("City");
+                String country = results.getString("Country");
+                String phone = results.getString("Phone");
+
+                System.out.printf("Contact:   %s %n Company:   %s %n City:   %s %n Country:   %s %n Phone:   %s %n", contactName, companyName, city, country, phone);
+                System.out.println("--------------------------------------------------------");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeConnection() throws SQLException {
+
+            if (results != null) {
+                try {
+                    results.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 }
